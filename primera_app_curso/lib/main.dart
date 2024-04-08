2ae9b486b047a99c1c64d73c8aa43ffa670aef18
jeftounz <srjesusc@gmail.com>
2024-04-07 21:00:20 -0400

    uso de columnas en el body

--- primera_app_curso/lib/main.dart
+++ primera_app_curso/lib/main.dart
@@ -33,24 +33,13 @@ class _InicioState  extends State<Inicio> {
       appBar: AppBar(
         title: Text("Mi App"),
       ),
-      body:ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network("https://i.guim.co.uk/img/media/2b8a584d7cffb01b95a41e3f7d596b0ab572ad07/0_250_5000_3000/master/5000.jpg?width=465&dpr=1&s=none"),
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network("https://www.irenebrination.com/.a/6a00e55290e7c4883302c8d3a58e29200b-800wi"),
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network("https://i.guim.co.uk/img/media/2b8a584d7cffb01b95a41e3f7d596b0ab572ad07/0_250_5000_3000/master/5000.jpg?width=465&dpr=1&s=none"),
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network("https://i.guim.co.uk/img/media/2b8a584d7cffb01b95a41e3f7d596b0ab572ad07/0_250_5000_3000/master/5000.jpg?width=465&dpr=1&s=none"),
-          ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text("Hola 01"),
+          Text("Hola 02"),
+          Text("Hola 03")
         ],
       )
     );
